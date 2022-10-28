ee5324d853b7aa91cee571a918a2c9fd5449f14a
Abe101 <cyrusrohail101@gmail.com>
2022-10-28 19:04:34 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='body' data-theme='forest'>
+      <div className='container m-5'>
+        <button className='btn btn-primary'>Click me!</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
